e1090fd6c428c36803bfc067d50ce7f251a7d12c
wfbeck-prog <wfbeck@gmail.com>
2026-04-30 14:06:38 -0700

    Add capture tick loop to MissionServer

--- EoH_WorldState/scripts/5_Mission/eoh_worldstate_init.c
+++ EoH_WorldState/scripts/5_Mission/eoh_worldstate_init.c
@@ -1,10 +1,30 @@
 modded class MissionServer
 {
+    protected int m_EoH_LastCaptureTick;
+
     override void OnInit()
     {
         super.OnInit();
 
         EoH_WorldStateManager.Get();
+        EoH_CaptureManager.Get();
+
+        m_EoH_LastCaptureTick = 0;
+
         Print("[EoH_WorldState] Initialized");
+    }
+
+    override void OnUpdate(float timeslice)
+    {
+        super.OnUpdate(timeslice);
+
+        int now = GetGame().GetTime();
+
+        // Run capture tick every 5 seconds
+        if (now - m_EoH_LastCaptureTick > 5000)
+        {
+            EoH_CaptureManager.Get().Tick();
+            m_EoH_LastCaptureTick = now;
+        }
     }
 };
