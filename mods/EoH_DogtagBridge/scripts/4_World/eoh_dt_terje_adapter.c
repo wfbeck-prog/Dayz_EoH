e4426aaccb58f51338cf195c2ad2885a2b64297d
wfbeck-prog <wfbeck@gmail.com>
2026-04-30 15:54:14 -0700

    Add Terje adapter placeholder

---
+++ mods/EoH_DogtagBridge/scripts/4_World/eoh_dt_terje_adapter.c
@@ -0,0 +1,9 @@
+class EoH_DT_TerjeAdapter
+{
+    // Placeholder until exact Terje API is confirmed
+    static int GetCombatSkill(PlayerBase player)
+    {
+        // TODO: Replace with real Terje API call
+        return 0;
+    }
+}
