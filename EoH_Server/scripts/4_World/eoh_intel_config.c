c2d93f6dcaeab878f564c28e2ab559e2a8180532
wfbeck-prog <wfbeck@gmail.com>
2026-05-03 13:33:52 -0700

    Add intel config (phase 1)

---
+++ EoH_Server/scripts/4_World/eoh_intel_config.c
@@ -0,0 +1,13 @@
+class EoH_IntelConfig
+{
+    int RevealDurationSeconds;
+    bool ShowOwnership;
+    bool ShowContested;
+
+    void EoH_IntelConfig()
+    {
+        RevealDurationSeconds = 300;
+        ShowOwnership = true;
+        ShowContested = true;
+    }
+};
