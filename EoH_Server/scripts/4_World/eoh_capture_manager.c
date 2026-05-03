ca0306d1ef72c94114a4c66ed18db3f12ad5d53f
wfbeck-prog <wfbeck@gmail.com>
2026-05-02 20:13:11 -0400

    Update eoh_capture_manager.c

--- EoH_Server/scripts/4_World/eoh_capture_manager.c
+++ EoH_Server/scripts/4_World/eoh_capture_manager.c
@@ -260,14 +260,14 @@ class EoH_CaptureManager
         if (m_Config.PauseWhenContested && session.EnemiesNearby > 0)
             session.IsContested = true;
 
-if (session.IsContested)
-{
-    EoH_TownMarkerManager.UpdateContestedMarker(session.TownName, session.AttackingGroupName);
-}
-else
-{
-    EoH_TownMarkerManager.ClearContestedMarker(session.TownName);
-}
+        if (session.IsContested)
+        {
+            EoH_TownMarkerManager.UpdateContestedMarker(session.TownName, session.AttackingGroupName);
+        }
+        else
+        {
+            EoH_TownMarkerManager.ClearContestedMarker(session.TownName);
+        }
     }
 
     protected bool CanProgress(EoH_CaptureSession session, EoH_CaptureTownConfig townCfg)
