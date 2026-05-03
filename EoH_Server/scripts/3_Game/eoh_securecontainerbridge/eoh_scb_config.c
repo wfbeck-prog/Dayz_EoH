fa52db3294b30b317cf5a878326fa0700b4dd5a6
wfbeck-prog <wfbeck@gmail.com>
2026-05-02 20:50:28 -0700

    Add RestoreDelayMs to EoH_SCB_Config to fix compile error

--- EoH_Server/scripts/3_Game/eoh_securecontainerbridge/eoh_scb_config.c
+++ EoH_Server/scripts/3_Game/eoh_securecontainerbridge/eoh_scb_config.c
@@ -3,11 +3,13 @@ class EoH_SCB_Config
     bool EnableDebug;
     int ContainerSize;
     bool PersistOnDeath;
+    int RestoreDelayMs;
 
     void EoH_SCB_Config()
     {
         EnableDebug = false;
         ContainerSize = 4;
         PersistOnDeath = true;
+        RestoreDelayMs = 5000;
     }
 };
