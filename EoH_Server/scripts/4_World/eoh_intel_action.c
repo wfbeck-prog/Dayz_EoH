828f1197eaee2a046e7d667e1761e3a221c77f60
wfbeck-prog <wfbeck@gmail.com>
2026-05-03 13:34:52 -0700

    Add intel use action

---
+++ EoH_Server/scripts/4_World/eoh_intel_action.c
@@ -0,0 +1,25 @@
+class ActionUseIntel: ActionInteractBase
+{
+    void ActionUseIntel()
+    {
+        m_CommandUID = DayZPlayerConstants.CMD_ACTIONMOD_INTERACTONCE;
+        m_StanceMask = DayZPlayerConstants.STANCEMASK_ALL;
+    }
+
+    override string GetText()
+    {
+        return "Use Intel";
+    }
+
+    override bool ActionCondition(PlayerBase player, ActionTarget target, ItemBase item)
+    {
+        return item && item.GetType() == "EoH_Intel_Document";
+    }
+
+    override void OnExecuteServer(ActionData action_data)
+    {
+        PlayerBase player = action_data.m_Player;
+        EoH_IntelManager.Get().RevealIntel(player);
+        action_data.m_MainItem.Delete();
+    }
+};
