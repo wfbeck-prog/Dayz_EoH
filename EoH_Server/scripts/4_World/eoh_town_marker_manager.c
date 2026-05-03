61488bc989ee65a0e6224af21100456664282d10
wfbeck-prog <wfbeck@gmail.com>
2026-05-03 13:45:27 -0700

    Add player-specific marker support for intel system

--- EoH_Server/scripts/4_World/eoh_town_marker_manager.c
+++ EoH_Server/scripts/4_World/eoh_town_marker_manager.c
@@ -22,54 +22,29 @@ class EoH_TownMarkerManager
         return ARGB(255, 200, 200, 200);
     }
 
-    static void SendMarkerToAll(EoH_TownMarkerData data)
+    static void SendMarkerToPlayer(PlayerBase player, EoH_TownMarkerData data)
     {
-        if (!GetGame() || !GetGame().IsServer())
+        if (!GetGame() || !GetGame().IsServer() || !player || !player.GetIdentity())
             return;
 
-        array<Man> players = new array<Man>();
-        GetGame().GetPlayers(players);
+        data.Normalize();
 
-        for (int i = 0; i < players.Count(); i++)
-        {
-            PlayerBase player = PlayerBase.Cast(players[i]);
-            if (!player || !player.GetIdentity())
-                continue;
-
-            Param1<ref EoH_TownMarkerData> param = new Param1<ref EoH_TownMarkerData>(data);
-            GetGame().RPCSingleParam(player, EoH_TownMarkerRPC.ADD_OR_UPDATE_TOWN_MARKER, param, true, player.GetIdentity());
-        }
+        Param1<ref EoH_TownMarkerData> param = new Param1<ref EoH_TownMarkerData>(data);
+        GetGame().RPCSingleParam(player, EoH_TownMarkerRPC.ADD_OR_UPDATE_TOWN_MARKER, param, true, player.GetIdentity());
     }
 
-    static void RemoveMarkerFromAll(string markerId)
+    static void RemoveMarkerFromPlayer(PlayerBase player, string markerId)
     {
-        if (!GetGame() || !GetGame().IsServer())
+        if (!GetGame() || !GetGame().IsServer() || !player || !player.GetIdentity())
             return;
 
-        array<Man> players = new array<Man>();
-        GetGame().GetPlayers(players);
-
-        for (int i = 0; i < players.Count(); i++)
-        {
-            PlayerBase player = PlayerBase.Cast(players[i]);
-            if (!player || !player.GetIdentity())
-                continue;
-
-            Param1<string> param = new Param1<string>(markerId);
-            GetGame().RPCSingleParam(player, EoH_TownMarkerRPC.REMOVE_TOWN_MARKER, param, true, player.GetIdentity());
-        }
+        Param1<string> param = new Param1<string>(markerId);
+        GetGame().RPCSingleParam(player, EoH_TownMarkerRPC.REMOVE_TOWN_MARKER, param, true, player.GetIdentity());
     }
 
     static string GetMarkerId(string townName)
     {
         string id = "EoH_TOWN_" + townName;
-        id.Replace(" ", "_");
-        return id;
-    }
-
-    static string GetContestedMarkerId(string townName)
-    {
-        string id = "EoH_CONTESTED_" + townName;
         id.Replace(" ", "_");
         return id;
     }
@@ -81,170 +56,5 @@ class EoH_TownMarkerManager
             return cfg.GetRelayVector();
 
         return "0 0 0";
-    }
-
-    static void UpdateTownMarker(string townName, string ownerGroupName)
-    {
-        vector pos = GetTownPosition(townName);
-        if (pos == "0 0 0")
-            return;
-
-        string id = GetMarkerId(townName);
-
-        EoH_TownMarkerData data = new EoH_TownMarkerData(id, townName, ownerGroupName, pos);
-        data.Color = GetGroupColor(ownerGroupName);
-        data.BaseColor = data.Color;
-
-        SendMarkerToAll(data);
-    }
-
-    static void UpdateContestedMarker(string townName, string attackingGroup)
-    {
-        vector pos = GetTownPosition(townName);
-        if (pos == "0 0 0")
-            return;
-
-        string id = GetContestedMarkerId(townName);
-
-        EoH_TownMarkerData data = new EoH_TownMarkerData(id, townName, attackingGroup, pos);
-        data.Color = ARGB(255, 255, 50, 50);
-        data.BaseColor = data.Color;
-        data.IsContested = 1;
-        data.Pulse = 1;
-
-        SendMarkerToAll(data);
-    }
-
-    static void ClearContestedMarker(string townName)
-    {
-        RemoveMarkerFromAll(GetContestedMarkerId(townName));
-    }
-};
-
-modded class PlayerBase
-{
-    override void OnRPC(PlayerIdentity sender, int rpc_type, ParamsReadContext ctx)
-    {
-        super.OnRPC(sender, rpc_type, ctx);
-
-        if (GetGame().IsServer())
-            return;
-
-        if (rpc_type == EoH_TownMarkerRPC.ADD_OR_UPDATE_TOWN_MARKER)
-        {
-            Param1<ref EoH_TownMarkerData> paramAdd;
-            if (!ctx.Read(paramAdd) || !paramAdd || !paramAdd.param1)
-                return;
-
-            EoH_TownMarkerClientManager.Get().SetMarker(paramAdd.param1);
-            return;
-        }
-
-        if (rpc_type == EoH_TownMarkerRPC.REMOVE_TOWN_MARKER)
-        {
-            Param1<string> paramRemove;
-            if (!ctx.Read(paramRemove) || !paramRemove)
-                return;
-
-            EoH_TownMarkerClientManager.Get().RemoveMarker(paramRemove.param1);
-            return;
-        }
-    }
-};
-
-class EoH_TownMarkerClientManager
-{
-    private static ref EoH_TownMarkerClientManager s_Instance;
-    protected ref map<string, ref EoH_TownMarkerData> m_Markers;
-    protected float m_PulseTime;
-
-    void EoH_TownMarkerClientManager()
-    {
-        m_Markers = new map<string, ref EoH_TownMarkerData>();
-        m_PulseTime = 0;
-
-        if (!GetGame().IsDedicatedServer())
-            GetGame().GetUpdateQueue(CALL_CATEGORY_GAMEPLAY).Insert(OnUpdate);
-    }
-
-    void ~EoH_TownMarkerClientManager()
-    {
-        if (!GetGame().IsDedicatedServer())
-            GetGame().GetUpdateQueue(CALL_CATEGORY_GAMEPLAY).Remove(OnUpdate);
-    }
-
-    static EoH_TownMarkerClientManager Get()
-    {
-        if (!s_Instance)
-            s_Instance = new EoH_TownMarkerClientManager();
-
-        return s_Instance;
-    }
-
-    void SetMarker(EoH_TownMarkerData data)
-    {
-        if (!data)
-            return;
-
-        m_Markers.Set(data.MarkerId, data);
-        ApplyMarker(data);
-    }
-
-    void ApplyMarker(EoH_TownMarkerData data)
-    {
-        ExpansionMarkerModule markerModule;
-        CF_Modules<ExpansionMarkerModule>.Get(markerModule);
-
-        if (!markerModule || !markerModule.GetData())
-            return;
-
-        ExpansionMarkerData marker = markerModule.GetData().PersonalGetByUID(data.MarkerId);
-
-        if (!marker)
-        {
-            marker = ExpansionMarkerData.Create(ExpansionMapMarkerType.PERSONAL, data.MarkerId);
-            marker.SetIcon(data.Icon);
-            markerModule.CreateMarker(marker);
-        }
-
-        marker.SetName(data.GetLabel());
-        marker.SetPosition(data.Position);
-        marker.SetColor(data.Color);
-
-        markerModule.Refresh();
-    }
-
-    void OnUpdate(float timeslice)
-    {
-        m_PulseTime += timeslice;
-
-        foreach (string id, EoH_TownMarkerData data : m_Markers)
-        {
-            if (!data || data.Pulse != 1)
-                continue;
-
-            float wave = Math.AbsFloat(Math.Sin(m_PulseTime * 5.0));
-
-            if (wave > 0.5)
-                data.Color = ARGB(255, 255, 255, 255);
-            else
-                data.Color = data.BaseColor;
-
-            ApplyMarker(data);
-        }
-    }
-
-    void RemoveMarker(string id)
-    {
-        if (id == "")
-            return;
-
-        m_Markers.Remove(id);
-
-        ExpansionMarkerModule markerModule;
-        CF_Modules<ExpansionMarkerModule>.Get(markerModule);
-
-        if (markerModule)
-            markerModule.RemovePersonalMarkerByUID(id);
     }
 };
