197b0487791a7d8853352ba38bc4c12732bee79d
wfbeck-prog <wfbeck@gmail.com>
2026-05-02 20:40:06 -0400

    Delete mods directory

--- mods/EoH_DogtagBridge/scripts/4_World/eoh_dt_terje_adapter.c
+++
@@ -1,9 +0,0 @@
-class EoH_DT_TerjeAdapter
-{
-    // Placeholder until exact Terje API is confirmed
-    static int GetCombatSkill(PlayerBase player)
-    {
-        // TODO: Replace with real Terje API call
-        return 0;
-    }
-}
