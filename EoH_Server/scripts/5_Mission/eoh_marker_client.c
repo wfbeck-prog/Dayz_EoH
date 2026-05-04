0cbba129f699691597e8b1b4e3c4cac4d55ae6ce
wfbeck-prog <wfbeck@gmail.com>
2026-05-04 15:14:48 -0700

    Upgrade marker system with color + pulse support (faint, intel, contested)

--- EoH_Server/scripts/5_Mission/eoh_marker_client.c
+++ EoH_Server/scripts/5_Mission/eoh_marker_client.c
@@ -1,13 +1,29 @@
+class EoH_ClientMarker
+{
+    vector Pos;
+    string Name;
+    int Color;
+    int BaseColor;
+    bool Pulse;
+};
+
 class EoH_ClientMarkers
 {
-    static ref array<ref Param3<vector, string, int>> m_Markers;
+    static ref array<ref EoH_ClientMarker> m_Markers;
 
-    static void Add(vector pos, string name, int color)
+    static void Add(vector pos, string name, int color, bool pulse = false)
     {
         if (!m_Markers)
-            m_Markers = new array<ref Param3<vector, string, int>>();
+            m_Markers = new array<ref EoH_ClientMarker>();
 
-        m_Markers.Insert(new Param3<vector, string, int>(pos, name, color));
+        EoH_ClientMarker m = new EoH_ClientMarker();
+        m.Pos = pos;
+        m.Name = name;
+        m.Color = color;
+        m.BaseColor = color;
+        m.Pulse = pulse;
+
+        m_Markers.Insert(m);
     }
 
     static void Clear()
@@ -16,17 +32,38 @@ class EoH_ClientMarkers
             m_Markers.Clear();
     }
 
+    static void UpdatePulse()
+    {
+        if (!m_Markers)
+            return;
+
+        int t = GetGame().GetTime() % 1000;
+
+        foreach (EoH_ClientMarker m : m_Markers)
+        {
+            if (!m.Pulse)
+                continue;
+
+            if (t < 500)
+                m.Color = ARGB(255, 255, 50, 50);
+            else
+                m.Color = ARGB(255, 255, 255, 255);
+        }
+    }
+
     static void Render(MapWidget map)
     {
         if (!map || !m_Markers)
             return;
 
-        foreach (Param3<vector, string, int> m : m_Markers)
+        UpdatePulse();
+
+        foreach (EoH_ClientMarker m : m_Markers)
         {
             map.AddUserMark(
-                m.param1,
-                m.param2,
-                m.param3,
+                m.Pos,
+                m.Name,
+                m.Color,
                 "\\dz\\gear\\navigation\\data\\map_tree_ca.paa"
             );
         }
