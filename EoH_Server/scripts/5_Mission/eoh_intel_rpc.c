91c17ca409f046cda63e2d2b67b504f69059336c
wfbeck-prog <wfbeck@gmail.com>
2026-05-04 15:15:23 -0700

    Add color + contested pulse support to intel RPC

--- EoH_Server/scripts/5_Mission/eoh_intel_rpc.c
+++ EoH_Server/scripts/5_Mission/eoh_intel_rpc.c
@@ -14,16 +14,25 @@ modded class MissionGameplay
 
         if (id == 777001)
         {
-            Param2<vector, string> p;
+            Param3<vector, string, int> p;
             if (rpc.GetReadContext().Read(p))
             {
-                EoH_ClientMarkers.Add(p.param1, p.param2, ARGB(255,255,255,255));
+                EoH_ClientMarkers.Add(p.param1, p.param2, p.param3, false);
             }
         }
 
         if (id == 777002)
         {
             EoH_ClientMarkers.Clear();
+        }
+
+        if (id == 777003)
+        {
+            Param2<vector, string> p2;
+            if (rpc.GetReadContext().Read(p2))
+            {
+                EoH_ClientMarkers.Add(p2.param1, p2.param2, ARGB(255,255,50,50), true);
+            }
         }
     }
 };
