25b0313d687058eb76dfa818ec1d71dd1278bee1
wfbeck-prog <wfbeck@gmail.com>
2026-05-05 08:59:40 -0700

    Remove invalid underground panel action override

--- EoH_Server/scripts/4_World/EoH_PunchedCard.c
+++ EoH_Server/scripts/4_World/EoH_PunchedCard.c
@@ -6,22 +6,3 @@ modded class PunchedCard : Paper
         AddAction(ActionUseUndergroundPanel);
     }
 }
-
-modded class ActionUseUndergroundPanel
-{
-    override void OnFinishProgressServer(ActionData action_data)
-    {
-        super.OnFinishProgressServer(action_data);
-
-        if (!action_data || !action_data.m_MainItem)
-            return;
-
-        PunchedCard card = PunchedCard.Cast(action_data.m_MainItem);
-        if (!card)
-            return;
-
-        card.AddHealthLevel(4);
-
-        EoH_WorldStateManager.Get().TriggerBunkerOpened();
-    }
-}
