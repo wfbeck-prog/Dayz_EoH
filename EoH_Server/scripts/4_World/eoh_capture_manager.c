c236a96be69047393944532c08ac9da0a620da64
wfbeck-prog <wfbeck@gmail.com>
2026-05-05 14:38:22 -0700

    Expand capture town registry for all EoH town markers

--- EoH_Server/scripts/4_World/eoh_capture_manager.c
+++ EoH_Server/scripts/4_World/eoh_capture_manager.c
@@ -43,10 +43,33 @@ class EoH_CaptureManager
 
     void InitTowns()
     {
+        m_Towns.Clear();
+
+        // Tier 1 towns
         m_Towns.Set("Pustoshka", "3060 0 7870".ToVector());
         m_Towns.Set("Mogilevka", "7600 0 5100".ToVector());
         m_Towns.Set("Guglovo", "8500 0 6600".ToVector());
+        m_Towns.Set("Tulga", "12750 0 4400".ToVector());
+        m_Towns.Set("Nadezhdino", "5850 0 4800".ToVector());
+        m_Towns.Set("Kamenka", "1850 0 2200".ToVector());
+
+        // Tier 2 towns
+        m_Towns.Set("Vybor", "3850 0 8900".ToVector());
+        m_Towns.Set("Stary Sobor", "6100 0 7800".ToVector());
         m_Towns.Set("Novy Sobor", "7000 0 7600".ToVector());
+        m_Towns.Set("Zelenogorsk", "2750 0 5300".ToVector());
+        m_Towns.Set("Staroye", "10150 0 5450".ToVector());
+        m_Towns.Set("Polana", "10700 0 8150".ToVector());
+
+        // Tier 3 cities
+        m_Towns.Set("Elektro", "10400 0 2250".ToVector());
+        m_Towns.Set("Chernogorsk", "6650 0 2550".ToVector());
+        m_Towns.Set("Berezino", "12250 0 9500".ToVector());
+
+        // Tier 4 military/high-risk zones
+        m_Towns.Set("NWAF", "4700 0 10300".ToVector());
+        m_Towns.Set("Tisy", "1675 0 14225".ToVector());
+        m_Towns.Set("Pavlovo Military", "2150 0 3350".ToVector());
     }
 
     vector GetTownPos(string town)
