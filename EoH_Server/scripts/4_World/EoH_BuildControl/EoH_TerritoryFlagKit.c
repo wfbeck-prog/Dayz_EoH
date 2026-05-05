ea0281dea5b2323710dfea9b69d6ac09e622ef22
wfbeck-prog <wfbeck@gmail.com>
2026-05-04 23:20:05 -0400

    Create EoH_TerritoryFlagKit.c

---
+++ EoH_Server/scripts/4_World/EoH_BuildControl/EoH_TerritoryFlagKit.c
@@ -0,0 +1,49 @@
+modded class TerritoryFlagKit extends KitBase
+{
+    override void OnPlacementComplete(Man player, vector position = "0 0 0", vector orientation = "0 0 0")
+    {
+        if (!GetGame().IsServer())
+            return;
+
+        PlayerBase playerBase = PlayerBase.Cast(player);
+        if (!playerBase)
+            return;
+
+        string groupID = EoH_GroupHelper.GetGroupID(playerBase);
+        if (groupID == "")
+        {
+            playerBase.MessageStatus("You must be in a group to place a territory flag.");
+            return;
+        }
+
+        TerritoryFlag placedFlag = TerritoryFlag.Cast(
+            GetGame().CreateObjectEx("TerritoryFlag", position, ECE_PLACE_ON_SURFACE)
+        );
+
+        if (!placedFlag)
+            return;
+
+        placedFlag.SetPosition(position);
+        placedFlag.SetOrientation(orientation);
+
+        HideAllSelections();
+        SetIsDeploySound(true);
+
+        placedFlag.GetConstruction().BuildPartServer(playerBase, "base", AT_BUILD_PART);
+        placedFlag.GetConstruction().BuildPartServer(playerBase, "support", AT_BUILD_PART);
+        placedFlag.GetConstruction().BuildPartServer(playerBase, "pole", AT_BUILD_PART);
+
+        if (!placedFlag.GetInventory().FindAttachment(
+            InventorySlots.GetSlotIdFromString("material_fpole_flag")))
+        {
+            placedFlag.GetInventory().CreateAttachment("Flag_DayZ");
+        }
+
+        placedFlag.GetConstruction().BuildPartServer(playerBase, "flag", AT_BUILD_PART);
+
+        // 🔥 Ownership assignment (critical)
+        placedFlag.SetEoHOwner(groupID);
+
+        playerBase.MessageStatus("Territory claimed for your group.");
+    }
+}
