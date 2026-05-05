3a8150429a21fb118bf946d1dcff7514f6a88ae0
wfbeck-prog <wfbeck@gmail.com>
2026-05-05 13:12:00 -0700

    Remove duplicate relay class from intel item definitions

--- EoH_Server/scripts/4_World/eoh_intel_item.c
+++ EoH_Server/scripts/4_World/eoh_intel_item.c
@@ -14,16 +14,3 @@ class EoH_TownIntel : EoH_Intel_Document
 class EoH_TraderIntel : EoH_Intel_Document
 {
 }
-
-class EoH_CaptureRelay_Base : ItemBase
-{
-    override void SetActions()
-    {
-        super.SetActions();
-        AddAction(EoH_ActionCaptureRelay);
-    }
-}
-
-class EoH_RadioRelay : EoH_CaptureRelay_Base
-{
-}
