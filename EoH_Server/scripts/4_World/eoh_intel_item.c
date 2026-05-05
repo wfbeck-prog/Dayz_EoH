a248079645955d1ffdcbc778f24872856f4767ef
wfbeck-prog <wfbeck@gmail.com>
2026-05-05 13:05:39 -0700

    Add concrete spawnable EoH intel and relay script classes

--- EoH_Server/scripts/4_World/eoh_intel_item.c
+++ EoH_Server/scripts/4_World/eoh_intel_item.c
@@ -1,8 +1,29 @@
-class EoH_Intel_Document : Inventory_Base
+class EoH_Intel_Document : ItemBase
 {
     override void SetActions()
     {
         super.SetActions();
-        AddAction(ActionUseIntel);
+        AddAction(EoH_ActionReadIntel);
     }
-};
+}
+
+class EoH_TownIntel : EoH_Intel_Document
+{
+}
+
+class EoH_TraderIntel : EoH_Intel_Document
+{
+}
+
+class EoH_CaptureRelay_Base : ItemBase
+{
+    override void SetActions()
+    {
+        super.SetActions();
+        AddAction(EoH_ActionCaptureRelay);
+    }
+}
+
+class EoH_RadioRelay : EoH_CaptureRelay_Base
+{
+}
