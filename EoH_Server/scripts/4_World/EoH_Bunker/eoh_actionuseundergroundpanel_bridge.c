016b9e320f9d6701df0c6be7e52b2cc8efb0fd40
wfbeck-prog <wfbeck@gmail.com>
2026-05-06 18:17:07 -0700

    Ruin punch card after underground panel use

--- EoH_Server/scripts/4_World/EoH_Bunker/eoh_actionuseundergroundpanel_bridge.c
+++ EoH_Server/scripts/4_World/EoH_Bunker/eoh_actionuseundergroundpanel_bridge.c
@@ -7,6 +7,21 @@ modded class ActionUseUndergroundPanel
         if (!action_data || !action_data.m_Player)
             return;
 
+        EoH_RuinPunchCard(action_data.m_MainItem);
         EoH_BunkerGlobalAlert.Send(action_data.m_Player);
+    }
+
+    void EoH_RuinPunchCard(ItemBase item)
+    {
+        if (!item)
+            return;
+
+        PunchedCard card = PunchedCard.Cast(item);
+        if (!card)
+            return;
+
+        card.SetHealth("", "Health", 0);
+        card.SetHealth01("", "Health", 0.0);
+        card.SetSynchDirty();
     }
 }
