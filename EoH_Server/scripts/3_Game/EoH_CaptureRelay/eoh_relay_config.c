d249278a75d72299fb2edaad0a2eb7d7c246eca3
wfbeck-prog <wfbeck@gmail.com>
2026-05-07 17:39:26 -0700

    Default relay startup cleanup to prevent stacking

--- EoH_Server/scripts/3_Game/EoH_CaptureRelay/eoh_relay_config.c
+++ EoH_Server/scripts/3_Game/EoH_CaptureRelay/eoh_relay_config.c
@@ -46,14 +46,14 @@ class EoH_RelayConfig
         Version = 1;
         Enabled = true;
         SpawnRelaysOnServerStart = true;
-        DeleteExistingEoHRelaysBeforeSpawn = false;
+        DeleteExistingEoHRelaysBeforeSpawn = true;
         Relays = new array<ref EoH_RelayLocation>();
     }
 
     void Defaults()
     {
         SpawnRelaysOnServerStart = true;
-        DeleteExistingEoHRelaysBeforeSpawn = false;
+        DeleteExistingEoHRelaysBeforeSpawn = true;
         Relays.Clear();
         AddRelay("Pustoshka", "3060 0 7870".ToVector(), 0);
         AddRelay("Mogilevka", "7600 0 5100".ToVector(), 0);
