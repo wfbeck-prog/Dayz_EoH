fbd3f6ce6b7594563549fee7660b3ad6c7507e5f
wfbeck-prog <wfbeck@gmail.com>
2026-05-07 17:23:23 -0700

    Convert bunker alert to shared EoH notification helper

--- EoH_Server/scripts/4_World/EoH_Bunker/eoh_bunker_global_alert.c
+++ EoH_Server/scripts/4_World/EoH_Bunker/eoh_bunker_global_alert.c
@@ -20,24 +20,13 @@ class EoH_BunkerGlobalAlert
         string title = "BUNKER ACCESS ALERT";
         string message = openerName + " has opened the underground bunker with a punch card.";
 
-        array<Man> players = new array<Man>();
-        GetGame().GetPlayers(players);
-
-        foreach (Man man : players)
-        {
-            PlayerBase player = PlayerBase.Cast(man);
-            if (!player || !player.GetIdentity())
-                continue;
-
-            PlayerIdentity identity = player.GetIdentity();
-
-            #ifdef EXPANSIONMODCORE
-            ExpansionNotification notification = new ExpansionNotification(title, message, "set:dayz_gui image:icon_skull", ARGB(255, 200, 25, 25), 10.0);
-            notification.Create(identity);
-            #else
-            player.MessageStatus(title + ": " + message);
-            #endif
-        }
+        EoH_Notifications.SendToAll(
+            title,
+            message,
+            "set:dayz_gui image:icon_skull",
+            ARGB(255, 200, 25, 25),
+            10.0
+        );
 
         Print("[EoH_Bunker] " + title + ": " + message);
     }
