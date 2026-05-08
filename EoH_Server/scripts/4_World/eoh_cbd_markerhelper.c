e834bd890445533cab8d77222aa613dad2ba1474
wfbeck-prog <wfbeck@gmail.com>
2026-05-07 18:11:24 -0700

    Normalize CBD keyroom marker IDs and flash behavior

--- EoH_Server/scripts/4_World/eoh_cbd_markerhelper.c
+++ EoH_Server/scripts/4_World/eoh_cbd_markerhelper.c
@@ -2,12 +2,49 @@ class EoH_CBD_MarkerHelper
 {
 	static string GetMarkerId(string roomName)
 	{
-		return "CBD_" + roomName;
+		return "KR_CBD_ROOM_" + roomName;
 	}
 
-	static int GetColor()
+	static int GetTierFromName(string roomName)
 	{
-		return ARGB(255, 255, 60, 60); // red
+		string upper = roomName;
+		upper.ToUpper();
+
+		if (upper.IndexOf("T1_") == 0 || upper.IndexOf("TIER1") != -1)
+			return 1;
+		if (upper.IndexOf("T2_") == 0 || upper.IndexOf("TIER2") != -1)
+			return 2;
+		if (upper.IndexOf("T3_") == 0 || upper.IndexOf("TIER3") != -1)
+			return 3;
+		if (upper.IndexOf("T4_") == 0 || upper.IndexOf("TIER4") != -1)
+			return 4;
+		if (upper.IndexOf("T5_") == 0 || upper.IndexOf("TIER5") != -1)
+			return 5;
+
+		return 0;
+	}
+
+	static int GetColor(int tier)
+	{
+		switch (tier)
+		{
+			case 1: return ARGB(255, 40, 200, 40);
+			case 2: return ARGB(255, 50, 120, 255);
+			case 3: return ARGB(255, 220, 180, 40);
+			case 4: return ARGB(255, 255, 120, 40);
+			case 5: return ARGB(255, 220, 40, 40);
+		}
+
+		return ARGB(255, 255, 60, 60);
+	}
+
+	static string GetLabel(LootSystemRoom room)
+	{
+		int tier = GetTierFromName(room.LootRoomName);
+		if (tier > 0)
+			return "CBD Loot Room T" + tier.ToString() + " OPEN";
+
+		return "CBD Loot Room OPEN";
 	}
 
 	static void Broadcast(LootSystemRoom room)
@@ -24,22 +61,31 @@ class EoH_CBD_MarkerHelper
 			if (!player || !player.GetIdentity())
 				continue;
 
-			EoH_RT_MarkerData data = new EoH_RT_MarkerData();
-
-			data.TraderId = "KR_" + GetMarkerId(room.LootRoomName);
-data.Label = room.LootRoomName;
-data.Position = room.LootRoomPosition;
-
-			data.Color = GetColor();
-			data.Pulse = 1;
-			data.Icon = "Danger";
-			data.Is3D = 1;
-
-			Param1<ref EoH_RT_MarkerData> markerParam;
-			markerParam = new Param1<ref EoH_RT_MarkerData>(data);
-
-			GetGame().RPCSingleParam(player, EoH_RT_RPC.ADD_OR_UPDATE_KEYROOM_MARKER, markerParam, true, player.GetIdentity());
+			SendToPlayer(player, room);
 		}
+
+		EoH_Notifications.SendToAll("CBD ROOM BREACHED", room.LootRoomName + " has been opened.");
+		Print("[EoH_CBD] Broadcast flash marker for room " + room.LootRoomName);
+	}
+
+	static void SendToPlayer(PlayerBase player, LootSystemRoom room)
+	{
+		if (!player || !player.GetIdentity() || !room)
+			return;
+
+		int tier = GetTierFromName(room.LootRoomName);
+
+		EoH_RT_MarkerData data = new EoH_RT_MarkerData();
+		data.TraderId = GetMarkerId(room.LootRoomName);
+		data.Label = GetLabel(room);
+		data.Position = room.LootRoomPosition;
+		data.Color = GetColor(tier);
+		data.Pulse = 1;
+		data.Icon = "Danger";
+		data.Is3D = 1;
+
+		Param1<ref EoH_RT_MarkerData> markerParam = new Param1<ref EoH_RT_MarkerData>(data);
+		GetGame().RPCSingleParam(player, EoH_RT_RPC.ADD_OR_UPDATE_KEYROOM_MARKER, markerParam, true, player.GetIdentity());
 	}
 
 	static void Clear(LootSystemRoom room)
@@ -47,8 +93,15 @@ data.Position = room.LootRoomPosition;
 		if (!room || !GetGame().IsServer())
 			return;
 
+		ClearByName(room.LootRoomName);
+	}
+
+	static void ClearByName(string roomName)
+	{
 		array<Man> players = new array<Man>();
 		GetGame().GetPlayers(players);
+
+		string markerId = GetMarkerId(roomName);
 
 		foreach (Man man : players)
 		{
@@ -56,10 +109,10 @@ data.Position = room.LootRoomPosition;
 			if (!player || !player.GetIdentity())
 				continue;
 
-			Param1<string> removeParam;
-			removeParam = new Param1<string>(GetMarkerId(room.LootRoomName));
-
+			Param1<string> removeParam = new Param1<string>(markerId);
 			GetGame().RPCSingleParam(player, EoH_RT_RPC.REMOVE_KEYROOM_MARKER, removeParam, true, player.GetIdentity());
 		}
+
+		Print("[EoH_CBD] Cleared marker " + markerId);
 	}
 }
