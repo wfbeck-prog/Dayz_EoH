4bd4c04ef1d76ad8100e6cf4f049fdc4e81b3c3d
wfbeck-prog <wfbeck@gmail.com>
2026-05-09 17:21:55 -0700

    Allow owned town relays as implied group spawn points

--- EoH_Server/scripts/4_World/EoH_Spawn/EoH_TerritorySpawn.c
+++ EoH_Server/scripts/4_World/EoH_Spawn/EoH_TerritorySpawn.c
@@ -25,22 +25,71 @@ class EoH_TerritorySpawn
         if (groupID == "")
             return false;
 
-        if (!m_Flags)
-            return false;
-
-        foreach (Flag_Base flag : m_Flags)
+        if (m_Flags)
         {
-            if (!flag)
-                continue;
-
-            if (flag.GetEoHOwner() == groupID)
+            foreach (Flag_Base flag : m_Flags)
             {
-                pos = flag.GetPosition();
-                return true;
+                if (!flag)
+                    continue;
+
+                if (flag.GetEoHOwner() == groupID)
+                {
+                    pos = flag.GetPosition();
+                    return true;
+                }
             }
         }
 
+        return GetOwnedTownSpawnPosition(player, pos);
+    }
+
+    static bool GetOwnedTownSpawnPosition(PlayerBase player, out vector pos)
+    {
+        if (!player || !player.GetIdentity())
+            return false;
+
+        string groupID = EoH_GroupHelper.GetGroupID(player);
+        if (groupID == "")
+            return false;
+
+        EoH_WorldStateData state = EoH_WorldStateManager.Get().GetState();
+        if (!state || !state.Towns)
+            return false;
+
+        foreach (EoH_WorldStateTownState town : state.Towns)
+        {
+            if (!town)
+                continue;
+
+            if (town.OwnerGroupID != groupID)
+                continue;
+
+            vector townPos = EoH_CaptureManager.Get().GetTownPos(town.Name);
+            if (townPos == "0 0 0".ToVector())
+                continue;
+
+            pos = GetSafeSpawn(townPos);
+            Print("[EoH_TerritorySpawn] Using owned town relay spawn for player=" + player.GetIdentity().GetName() + " town=" + town.Name + " group=" + town.OwnerGroupName + " pos=" + pos.ToString());
+            return true;
+        }
+
         return false;
+    }
+
+    static bool PlayerOwnsTown(PlayerBase player, string townName)
+    {
+        if (!player || !player.GetIdentity() || townName == "")
+            return false;
+
+        string groupID = EoH_GroupHelper.GetGroupID(player);
+        if (groupID == "")
+            return false;
+
+        EoH_WorldStateTownState town = EoH_WorldStateManager.Get().GetTownState(townName);
+        if (!town)
+            return false;
+
+        return town.OwnerGroupID == groupID;
     }
 
     static vector GetSafeSpawn(vector flagPos)
