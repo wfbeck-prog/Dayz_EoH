1c457eb7d14e0402cf1a91e59e1fb251fb740aa2
wfbeck-prog <wfbeck@gmail.com>
2026-05-09 20:48:13 -0400

    Update eoh_dt_core.c

--- EoH_Server/scripts/4_World/eoh_dt_core.c
+++ EoH_Server/scripts/4_World/eoh_dt_core.c
@@ -2,16 +2,16 @@ class EoH_DT_Core
 {
     static string ResolveDogtagClass(int exp)
     {
-        if (exp >= 2000)
+        if (exp >= 3000)
             return "Dogtag_Hero";
 
-        if (exp >= 1500)
+        if (exp >= 2000)
             return "Dogtag_Cobalt";
 
-        if (exp >= 1000)
+        if (exp >= 1500)
             return "Dogtag_Granite";
 
-        if (exp >= 500)
+        if (exp >= 1000)
             return "Dogtag_Silver";
 
         return "Dogtag_Survivor";
