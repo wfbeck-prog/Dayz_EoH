27d09071faed52dc33ca376f3a3801dcd746ac78
wfbeck-prog <wfbeck@gmail.com>
2026-05-12 14:32:59 -0700

    Use cache category and active state for DNA markers

--- EoH_Server/scripts/4_World/EoH_LootIntel/eoh_dna_cache_markerhelper.c
+++ EoH_Server/scripts/4_World/EoH_LootIntel/eoh_dna_cache_markerhelper.c
@@ -43,8 +43,8 @@ class EoH_DNACacheMarkerHelper
 	static EoH_MarkerData BuildCacheOpenedMarker(string tier, vector position)
 	{
 		EoH_MarkerData data = new EoH_MarkerData(BuildMarkerId(tier, position), GetMarkerLabel(tier), position);
-		data.Category = EoH_MarkerCategory.TRADER;
-		data.State = EoH_MarkerState.NORMAL;
+		data.Category = EoH_MarkerCategory.CACHE;
+		data.State = EoH_MarkerState.ACTIVE;
 		data.Icon = "Questionmark";
 		data.Is3D = 1;
 		data.Pulse = 1;
