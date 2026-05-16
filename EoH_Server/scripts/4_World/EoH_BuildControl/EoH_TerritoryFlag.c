0e7456a9a19aa65532a43f8d697398983b136681
wfbeck-prog <wfbeck@gmail.com>
2026-05-15 18:20:07 -0700

    Scale deployed territory flag to quarter size

--- EoH_Server/scripts/4_World/EoH_BuildControl/EoH_TerritoryFlag.c
+++ EoH_Server/scripts/4_World/EoH_BuildControl/EoH_TerritoryFlag.c
@@ -1,5 +1,12 @@
 modded class TerritoryFlag
 {
+    override void EEInit()
+    {
+        super.EEInit();
+
+        SetScale(0.25);
+    }
+
     override string GetConstructionKitType()
     {
         return string.Empty;
