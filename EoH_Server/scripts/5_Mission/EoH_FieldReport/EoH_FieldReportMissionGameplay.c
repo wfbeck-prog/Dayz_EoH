375d5e23211e8e671524f5efbc13b222fdf9695d
wfbeck-prog <wfbeck@gmail.com>
2026-05-19 15:07:38 -0700

    Handle field report RPCs in mission gameplay layer

---
+++ EoH_Server/scripts/5_Mission/EoH_FieldReport/EoH_FieldReportMissionGameplay.c
@@ -0,0 +1,19 @@
+modded class MissionGameplay
+{
+    override void OnRPC(PlayerIdentity sender, Object target, int rpc_type, ParamsReadContext ctx)
+    {
+        super.OnRPC(sender, target, rpc_type, ctx);
+
+        if (rpc_type == EoH_FieldReportRPC.OPEN_REPORT)
+        {
+            Param1<ref EoH_FieldReportData> data;
+            if (!ctx.Read(data))
+                return;
+
+            if (!data || !data.param1)
+                return;
+
+            EoH_FieldReportMenu.Open(data.param1);
+        }
+    }
+};
