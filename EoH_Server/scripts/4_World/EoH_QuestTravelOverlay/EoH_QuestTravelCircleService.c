e6760630bb4af57a912218912a0c2f13c7c220e7
wfbeck-prog <wfbeck@gmail.com>
2026-05-19 09:16:37 -0700

    Add personal quest travel circle RPC service

---
+++ EoH_Server/scripts/4_World/EoH_QuestTravelOverlay/EoH_QuestTravelCircleService.c
@@ -0,0 +1,33 @@
+class EoH_QuestTravelCircleService
+{
+    static void SendToPlayer(PlayerBase player, EoH_QuestTravelOverlayData overlay)
+    {
+        if (!player || !player.GetIdentity() || !overlay)
+            return;
+
+        EoH_QuestTravelCircleData data = new EoH_QuestTravelCircleData(
+            overlay.GetId(),
+            overlay.Label,
+            overlay.CircleCenter,
+            overlay.Radius
+        );
+
+        data.Normalize();
+
+        Param1<ref EoH_QuestTravelCircleData> param = new Param1<ref EoH_QuestTravelCircleData>(data);
+        GetGame().RPCSingleParam(player, EoH_QuestTravelCircleRPC.ADD_OR_UPDATE_CIRCLE, param, true, player.GetIdentity());
+
+        Print("[EoH_QuestCircle] Sent personal search circle player=" + player.GetIdentity().GetName() + " id=" + data.Id + " center=" + data.Center.ToString() + " radius=" + data.Radius.ToString());
+    }
+
+    static void RemoveFromPlayer(PlayerBase player, string id)
+    {
+        if (!player || !player.GetIdentity() || id == "")
+            return;
+
+        Param1<string> param = new Param1<string>(id);
+        GetGame().RPCSingleParam(player, EoH_QuestTravelCircleRPC.REMOVE_CIRCLE, param, true, player.GetIdentity());
+
+        Print("[EoH_QuestCircle] Removed personal search circle player=" + player.GetIdentity().GetName() + " id=" + id);
+    }
+};
