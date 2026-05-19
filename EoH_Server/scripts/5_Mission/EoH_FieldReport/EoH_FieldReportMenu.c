23e676281bcef74cf506c4d291691ecf74c1324c
wfbeck-prog <wfbeck@gmail.com>
2026-05-19 15:19:41 -0700

    Remove incompatible field report keypress override

--- EoH_Server/scripts/5_Mission/EoH_FieldReport/EoH_FieldReportMenu.c
+++ EoH_Server/scripts/5_Mission/EoH_FieldReport/EoH_FieldReportMenu.c
@@ -60,14 +60,6 @@ class EoH_FieldReportMenu extends UIScriptedMenu
         return super.OnClick(w, x, y, button);
     }
 
-    override void OnKeyPress(Widget w, int x, int y, int key)
-    {
-        super.OnKeyPress(w, x, y, key);
-
-        if (key == KeyCode.KC_ESCAPE)
-            Close();
-    }
-
     void Close()
     {
         GetGame().GetUIManager().Back();
