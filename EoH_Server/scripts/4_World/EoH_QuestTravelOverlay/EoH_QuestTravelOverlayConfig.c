fadeb540b877a908c9e7e0aa45184979a6daf2f0
wfbeck-prog <wfbeck@gmail.com>
2026-05-19 05:01:36 -0700

    Generalize quest travel overlay config

--- EoH_Server/scripts/4_World/EoH_QuestTravelOverlay/EoH_QuestTravelOverlayConfig.c
+++ EoH_Server/scripts/4_World/EoH_QuestTravelOverlay/EoH_QuestTravelOverlayConfig.c
@@ -9,17 +9,23 @@ class EoH_QuestTravelOverlayConfig
 
         s_Overlays = new array<ref EoH_QuestTravelOverlayData>();
 
-        // Prototype: Signal and Ashes: Dead Air 1
-        EoH_QuestTravelOverlayData deadAir = new EoH_QuestTravelOverlayData();
-        deadAir.QuestID = 401001;
-        deadAir.ObjectiveID = 501001;
-        deadAir.Label = "Signal and Ashes: Search Area";
-        deadAir.TruePosition = "3060 310 7870".ToVector();
-        deadAir.Radius = 350.0;
-        deadAir.CircleCenter = EoH_QuestTravelOverlayData.ComputeOffsetCenter(deadAir.TruePosition, deadAir.QuestID, deadAir.ObjectiveID, deadAir.Radius);
-        s_Overlays.Insert(deadAir);
+        // Signal and Ashes: Dead Air 1
+        AddOverlay(401001, 501001, "Signal and Ashes: Search Area", "3060 310 7870".ToVector(), 350.0);
 
-        Print("[EoH_TravelOverlay] Loaded prototype overlays=" + s_Overlays.Count().ToString());
+        Print("[EoH_TravelOverlay] Loaded quest travel overlays=" + s_Overlays.Count().ToString());
+    }
+
+    static void AddOverlay(int questId, int objectiveId, string label, vector truePosition, float radius)
+    {
+        EoH_QuestTravelOverlayData data = new EoH_QuestTravelOverlayData(questId, objectiveId, label, truePosition, radius);
+        s_Overlays.Insert(data);
+        Print("[EoH_TravelOverlay] Config overlay quest=" + questId.ToString() + " objective=" + objectiveId.ToString() + " label=" + label + " center=" + data.CircleCenter.ToString() + " radius=" + radius.ToString());
+    }
+
+    static array<ref EoH_QuestTravelOverlayData> GetAll()
+    {
+        Init();
+        return s_Overlays;
     }
 
     static EoH_QuestTravelOverlayData GetByObjective(int objectiveId)
@@ -33,5 +39,18 @@ class EoH_QuestTravelOverlayConfig
         }
 
         return null;
+    }
+
+    static bool HasQuest(int questId)
+    {
+        Init();
+
+        foreach (EoH_QuestTravelOverlayData data : s_Overlays)
+        {
+            if (data && data.QuestID == questId)
+                return true;
+        }
+
+        return false;
     }
 };
