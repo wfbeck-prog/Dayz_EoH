b5a433ae4ad1b7823c3c4082932b9e62adb24b50
wfbeck-prog <wfbeck@gmail.com>
2026-05-21 13:49:32 -0700

    Log owned respawn registry during town respawn validation

--- EoH_Server/scripts/4_World/EoH_Terje/EoH_TerjeTownRespawnHandler.c
+++ EoH_Server/scripts/4_World/EoH_Terje/EoH_TerjeTownRespawnHandler.c
@@ -2,6 +2,8 @@ class EoH_TerjeTownRespawnHandler : TerjeRespawnObjectHandler
 {
     override bool SetAsRespawnObject(PlayerBase player, Object object, string respawnId)
     {
+        EoH_DebugOwnedRespawns(player, "SetAsRespawnObject", respawnId);
+
         // EoH town relay respawns are ownership-implied.
         // Players should not manually mark radio relays as respawn points.
         return false;
@@ -9,7 +11,17 @@ class EoH_TerjeTownRespawnHandler : TerjeRespawnObjectHandler
 
     override bool RespawnOnObject(PlayerBase player, Object object, string respawnId)
     {
+        EoH_DebugOwnedRespawns(player, "RespawnOnObject", respawnId);
         return EoH_IsObjectValid(object, player, respawnId);
+    }
+
+    void EoH_DebugOwnedRespawns(PlayerBase player, string phase, string respawnId)
+    {
+        if (!player || !player.GetIdentity())
+            return;
+
+        ref array<ref EoH_TerjeOwnedRespawnEntry> entries = EoH_TerjeOwnedRespawnRegistry.BuildOwnedRespawns(player);
+        Print("[EoH_TerjeRespawn][TOWN_HANDLER] phase=" + phase + " respawnId=" + respawnId + " player=" + player.GetIdentity().GetName() + " registryCount=" + entries.Count().ToString());
     }
 
     bool EoH_IsObjectValid(Object object, PlayerBase player, string respawnId)
