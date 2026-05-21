bdd66eff831e24d0a1ac2de051d248ee658215e0
wfbeck-prog <wfbeck@gmail.com>
2026-05-21 13:31:38 -0700

    Delay town reward smoke particle startup on client

--- EoH_Server/scripts/4_World/EoH_TownReward/eoh_reward_smoke_marker.c
+++ EoH_Server/scripts/4_World/EoH_TownReward/eoh_reward_smoke_marker.c
@@ -4,10 +4,29 @@ class EoH_RewardSmokeMarker extends House
 
     void EoH_RewardSmokeMarker()
     {
-        if (!GetGame().IsDedicatedServer())
-        {
-            m_EoH_SmokeParticle = Particle.PlayOnObject(ParticleList.EXPANSION_AIRDROP_SMOKE, this);
-        }
+        SetEventMask(EntityEvent.INIT);
+    }
+
+    override void EOnInit(IEntity other, int extra)
+    {
+        super.EOnInit(other, extra);
+
+        if (!GetGame() || GetGame().IsDedicatedServer())
+            return;
+
+        GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(EoH_StartSmoke, 250, false);
+    }
+
+    void EoH_StartSmoke()
+    {
+        if (!GetGame() || GetGame().IsDedicatedServer())
+            return;
+
+        if (m_EoH_SmokeParticle)
+            return;
+
+        m_EoH_SmokeParticle = Particle.PlayOnObject(ParticleList.EXPANSION_AIRDROP_SMOKE, this, "0 0.5 0".ToVector());
+        Print("[EoH_TownReward][SMOKE] Client started reward smoke particle at " + GetPosition().ToString());
     }
 
     void ~EoH_RewardSmokeMarker()
