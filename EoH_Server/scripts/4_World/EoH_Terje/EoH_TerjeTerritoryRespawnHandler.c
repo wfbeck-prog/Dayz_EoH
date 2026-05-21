673b63bedc8a909d1832a4aee41a7c5b15527df4
wfbeck-prog <wfbeck@gmail.com>
2026-05-21 13:50:13 -0700

    Log owned respawn registry during territory respawn validation

--- EoH_Server/scripts/4_World/EoH_Terje/EoH_TerjeTerritoryRespawnHandler.c
+++ EoH_Server/scripts/4_World/EoH_Terje/EoH_TerjeTerritoryRespawnHandler.c
@@ -2,12 +2,23 @@ class EoH_TerjeTerritoryRespawnHandler : TerjeRespawnObjectHandler
 {
     override bool SetAsRespawnObject(PlayerBase player, Object object, string respawnId)
     {
+        EoH_DebugOwnedRespawns(player, "SetAsRespawnObject", respawnId);
         return EoH_IsObjectValid(object, player, respawnId);
     }
 
     override bool RespawnOnObject(PlayerBase player, Object object, string respawnId)
     {
+        EoH_DebugOwnedRespawns(player, "RespawnOnObject", respawnId);
         return EoH_IsObjectValid(object, player, respawnId);
+    }
+
+    void EoH_DebugOwnedRespawns(PlayerBase player, string phase, string respawnId)
+    {
+        if (!player || !player.GetIdentity())
+            return;
+
+        ref array<ref EoH_TerjeOwnedRespawnEntry> entries = EoH_TerjeOwnedRespawnRegistry.BuildOwnedRespawns(player);
+        Print("[EoH_TerjeRespawn][TERRITORY_HANDLER] phase=" + phase + " respawnId=" + respawnId + " player=" + player.GetIdentity().GetName() + " registryCount=" + entries.Count().ToString());
     }
 
     bool EoH_IsObjectValid(Object object, PlayerBase player, string respawnId)
