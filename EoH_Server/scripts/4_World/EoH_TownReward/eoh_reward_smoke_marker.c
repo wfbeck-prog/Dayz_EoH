56b05d49324f08ca2901d255728b35d0aa4f37d1
wfbeck-prog <wfbeck@gmail.com>
2026-05-22 08:48:38 -0700

    Add repeated client smoke startup checks for reward marker

--- EoH_Server/scripts/4_World/EoH_TownReward/eoh_reward_smoke_marker.c
+++ EoH_Server/scripts/4_World/EoH_TownReward/eoh_reward_smoke_marker.c
@@ -1,20 +1,29 @@
 class EoH_RewardSmokeMarker extends House
 {
     protected Particle m_EoH_SmokeParticle;
+    protected int m_EoH_SmokeAttempts;
 
     void EoH_RewardSmokeMarker()
     {
+        m_EoH_SmokeAttempts = 0;
         SetEventMask(EntityEvent.INIT);
     }
 
     override void EOnInit(IEntity other, int extra)
     {
         super.EOnInit(other, extra);
+        EoH_ScheduleSmokeStart();
+    }
 
+    void EoH_ScheduleSmokeStart()
+    {
         if (!GetGame() || GetGame().IsDedicatedServer())
             return;
 
         GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(EoH_StartSmoke, 250, false);
+        GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(EoH_StartSmoke, 1000, false);
+        GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(EoH_StartSmoke, 2500, false);
+        GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(EoH_StartSmoke, 5000, false);
     }
 
     void EoH_StartSmoke()
@@ -22,11 +31,23 @@ class EoH_RewardSmokeMarker extends House
         if (!GetGame() || GetGame().IsDedicatedServer())
             return;
 
+        m_EoH_SmokeAttempts++;
+
         if (m_EoH_SmokeParticle)
             return;
 
-        m_EoH_SmokeParticle = Particle.PlayOnObject(ParticleList.EXPANSION_AIRDROP_SMOKE, this, "0 0.5 0".ToVector());
-        Print("[EoH_TownReward][SMOKE] Client started reward smoke particle at " + GetPosition().ToString());
+        vector pos = GetPosition();
+        if (pos == "0 0 0".ToVector())
+        {
+            Print("[EoH_TownReward][SMOKE][WARN] Reward smoke marker client has invalid position on attempt=" + m_EoH_SmokeAttempts.ToString());
+            return;
+        }
+
+        m_EoH_SmokeParticle = Particle.PlayOnObject(ParticleList.EXPANSION_AIRDROP_SMOKE, this, "0 1.0 0".ToVector());
+        if (m_EoH_SmokeParticle)
+            Print("[EoH_TownReward][SMOKE] Client started reward smoke particle attempt=" + m_EoH_SmokeAttempts.ToString() + " pos=" + pos.ToString());
+        else
+            Print("[EoH_TownReward][SMOKE][WARN] Particle.PlayOnObject returned null attempt=" + m_EoH_SmokeAttempts.ToString() + " pos=" + pos.ToString());
     }
 
     void ~EoH_RewardSmokeMarker()
