1d985e1bbefda61d82f5e14a7dfff909e6a436a4
wfbeck-prog <wfbeck@gmail.com>
2026-05-24 09:03:31 -0700

    Throttle server heartbeat systems for performance

--- EoH_Server/scripts/5_Mission/EoH_WeeklyEvents/eoh_weeklyevents_missionserver.c
+++ EoH_Server/scripts/5_Mission/EoH_WeeklyEvents/eoh_weeklyevents_missionserver.c
@@ -1,11 +1,23 @@
 modded class MissionServer
 {
+    protected int m_EoH_HeartbeatLastRun;
+    protected const int EOH_HEARTBEAT_INTERVAL_MS = 60000;
+
     override void OnUpdate(float timeslice)
     {
         super.OnUpdate(timeslice);
 
+        int now = GetGame().GetTime();
+        if (m_EoH_HeartbeatLastRun > 0 && now - m_EoH_HeartbeatLastRun < EOH_HEARTBEAT_INTERVAL_MS)
+            return;
+
+        m_EoH_HeartbeatLastRun = now;
+
         EoH_WeeklyEventManager.Get().Tick();
-        EoH_GroupFlagMarkerManager.Tick();
+
+        // Disabled temporarily for live-server performance recovery.
+        // This scans TerritoryFlag objects and should be reworked to use cached flag registration.
+        // EoH_GroupFlagMarkerManager.Tick();
 
         // Disabled for live-server performance recovery.
         // The DNA relocator currently performs expensive world object scans and caused severe FPS drops.
