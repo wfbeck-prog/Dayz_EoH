e82787f4ece418ef14691a22caae8ef086f67a8e
wfbeck-prog <wfbeck@gmail.com>
2026-05-26 15:18:23 -0700

    Add Live Advisor server performance monitor

---
+++ EoH_Server/scripts/5_Mission/EoH_LiveAdvisor/EoH_LiveAdvisorPerformance.c
@@ -0,0 +1,79 @@
+class EoH_LiveAdvisorPerformance
+{
+    protected static float m_LastPerformanceWarningTime;
+
+    static void CheckServerPerformance(float timeslice)
+    {
+        if (!EoH_LiveAdvisorLogger.m_Config)
+            return;
+
+        if (!EoH_LiveAdvisorLogger.m_Config.EnablePerformanceMonitor)
+            return;
+
+        float fps = 0;
+
+        if (timeslice > 0)
+        {
+            fps = 1.0 / timeslice;
+        }
+
+        float now = GetGame().GetTime() / 1000.0;
+        float cooldown = EoH_LiveAdvisorLogger.m_Config.PerformanceWarningCooldownSeconds;
+
+        if (m_LastPerformanceWarningTime > 0 && now - m_LastPerformanceWarningTime < cooldown)
+        {
+            return;
+        }
+
+        if (fps <= EoH_LiveAdvisorLogger.m_Config.CriticalFpsThreshold)
+        {
+            m_LastPerformanceWarningTime = now;
+
+            string criticalMessage = string.Format(
+                "CRITICAL server FPS detected. FPS=%1 Timeslice=%2 ActivePlayers=%3",
+                fps.ToString(),
+                timeslice.ToString(),
+                EoH_GetPlayerCount().ToString()
+            );
+
+            EoH_LiveAdvisorLogger.Log(
+                "SERVER_FPS_CRITICAL",
+                criticalMessage,
+                "critical",
+                "EoH_Performance"
+            );
+
+            return;
+        }
+
+        if (fps <= EoH_LiveAdvisorLogger.m_Config.LowFpsWarningThreshold)
+        {
+            m_LastPerformanceWarningTime = now;
+
+            string warningMessage = string.Format(
+                "Low server FPS detected. FPS=%1 Timeslice=%2 ActivePlayers=%3",
+                fps.ToString(),
+                timeslice.ToString(),
+                EoH_GetPlayerCount().ToString()
+            );
+
+            EoH_LiveAdvisorLogger.Log(
+                "SERVER_FPS_WARNING",
+                warningMessage,
+                "warning",
+                "EoH_Performance"
+            );
+        }
+    }
+
+    static int EoH_GetPlayerCount()
+    {
+        array<Man> players = new array<Man>;
+        GetGame().GetPlayers(players);
+
+        if (!players)
+            return 0;
+
+        return players.Count();
+    }
+}
