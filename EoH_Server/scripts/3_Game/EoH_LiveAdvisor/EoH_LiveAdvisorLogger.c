2fba05af99f2ca5aba94b4f43734c78625304a0c
wfbeck-prog <wfbeck@gmail.com>
2026-05-26 11:23:09 -0700

    Add report generation for web console hosting

--- EoH_Server/scripts/3_Game/EoH_LiveAdvisor/EoH_LiveAdvisorLogger.c
+++ EoH_Server/scripts/3_Game/EoH_LiveAdvisor/EoH_LiveAdvisorLogger.c
@@ -3,8 +3,10 @@ class EoH_LiveAdvisorLogger
     static const string PROFILE_FOLDER = "$profile:EoH/LiveAdvisor/";
     static const string SETTINGS_FILE = "$profile:EoH/LiveAdvisor/LiveAdvisorSettings.json";
     static const string EVENTS_FILE = "$profile:EoH/LiveAdvisor/events.log";
+    static const string REPORT_FILE = "$profile:EoH/LiveAdvisor/latest_report.txt";
 
     static ref EoH_LiveAdvisorConfig m_Config;
+    static ref array<string> m_RecentEvents;
 
     static void Init()
     {
@@ -23,6 +25,11 @@ class EoH_LiveAdvisorLogger
             JsonFileLoader<EoH_LiveAdvisorConfig>.JsonLoadFile(SETTINGS_FILE, m_Config);
         }
 
+        if (!m_RecentEvents)
+        {
+            m_RecentEvents = new array<string>();
+        }
+
         Log("SERVER_START", "EoH Live Advisor initialized", "info");
     }
 
@@ -39,9 +46,50 @@ class EoH_LiveAdvisorLogger
             CloseFile(file);
         }
 
+        if (!m_RecentEvents)
+        {
+            m_RecentEvents = new array<string>();
+        }
+
+        m_RecentEvents.Insert(line);
+
+        if (m_Config && m_RecentEvents.Count() > m_Config.MaxReportLines)
+        {
+            m_RecentEvents.RemoveOrdered(0);
+        }
+
+        WriteReport();
+
         if (m_Config && m_Config.LogDebugToRPT)
         {
             Print("[EoH_LiveAdvisor] " + line);
         }
+    }
+
+    static void WriteReport()
+    {
+        if (!m_Config || !m_Config.WriteCopyPasteReport)
+        {
+            return;
+        }
+
+        FileHandle report = OpenFile(REPORT_FILE, FileMode.WRITE);
+
+        if (!report)
+        {
+            return;
+        }
+
+        FPrintln(report, "=== Echoes of Humanity Live Advisor Report ===");
+        FPrintln(report, "Server: " + m_Config.ServerName);
+        FPrintln(report, "Mode: Advisor Only");
+        FPrintln(report, " ");
+
+        foreach (string line : m_RecentEvents)
+        {
+            FPrintln(report, line);
+        }
+
+        CloseFile(report);
     }
 }
