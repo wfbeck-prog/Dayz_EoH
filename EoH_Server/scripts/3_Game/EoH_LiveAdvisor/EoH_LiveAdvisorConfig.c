25308301491229a2ebebf9ae45f5ff04bd14a88f
wfbeck-prog <wfbeck@gmail.com>
2026-05-26 11:22:51 -0700

    Make Live Advisor web-console friendly

--- EoH_Server/scripts/3_Game/EoH_LiveAdvisor/EoH_LiveAdvisorConfig.c
+++ EoH_Server/scripts/3_Game/EoH_LiveAdvisor/EoH_LiveAdvisorConfig.c
@@ -1,21 +1,25 @@
 class EoH_LiveAdvisorConfig
 {
-    int ConfigVersion = 1;
+    int ConfigVersion = 2;
     bool Enabled = true;
     bool AdvisorOnly = true;
     string ServerName = "Echoes of Humanity Hardcore";
     int HeartbeatSeconds = 300;
     bool LogPlayerCounts = true;
     bool LogDebugToRPT = true;
+    bool WriteCopyPasteReport = true;
+    int MaxReportLines = 75;
 
     void Defaults()
     {
-        ConfigVersion = 1;
+        ConfigVersion = 2;
         Enabled = true;
         AdvisorOnly = true;
         ServerName = "Echoes of Humanity Hardcore";
         HeartbeatSeconds = 300;
         LogPlayerCounts = true;
         LogDebugToRPT = true;
+        WriteCopyPasteReport = true;
+        MaxReportLines = 75;
     }
 }
