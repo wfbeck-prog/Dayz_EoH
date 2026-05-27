8f53f52d904c0897eea5d294ec7216c62638b127
wfbeck-prog <wfbeck@gmail.com>
2026-05-26 17:00:06 -0700

    Use average FPS and worst-frame metrics for performance samples

--- EoH_Server/scripts/5_Mission/EoH_LiveAdvisor/EoH_LiveAdvisorPerformance.c
+++ EoH_Server/scripts/5_Mission/EoH_LiveAdvisor/EoH_LiveAdvisorPerformance.c
@@ -2,6 +2,9 @@ class EoH_LiveAdvisorPerformance
 {
     protected static float m_LastPerformanceWarningTime;
     protected static float m_SampleTimer;
+    protected static float m_AccumulatedTime;
+    protected static int m_FrameCount;
+    protected static float m_WorstTimeslice;
 
     static void CheckServerPerformance(float timeslice)
     {
@@ -12,56 +15,69 @@ class EoH_LiveAdvisorPerformance
             return;
 
         m_SampleTimer += timeslice;
+        m_AccumulatedTime += timeslice;
+        m_FrameCount++;
+
+        if (timeslice > m_WorstTimeslice)
+        {
+            m_WorstTimeslice = timeslice;
+        }
 
         if (m_SampleTimer < EoH_LiveAdvisorLogger.m_Config.PerformanceSampleSeconds)
             return;
 
-        m_SampleTimer = 0;
+        float avgFps = 0;
+        float worstFrameFps = 0;
 
-        float fps = 0;
-
-        if (timeslice > 0)
+        if (m_AccumulatedTime > 0 && m_FrameCount > 0)
         {
-            fps = 1.0 / timeslice;
+            avgFps = m_FrameCount / m_AccumulatedTime;
+        }
+
+        if (m_WorstTimeslice > 0)
+        {
+            worstFrameFps = 1.0 / m_WorstTimeslice;
         }
 
         int players = EoH_GetPlayerCount();
 
         if (EoH_LiveAdvisorLogger.m_Config.LogPerformanceSamples)
         {
-            string sampleMessage = "FPS=" + fps.ToString() + " Players=" + players.ToString() + " Timeslice=" + timeslice.ToString();
+            string sampleMessage = "AvgFPS=" + avgFps.ToString() + " WorstFrameFPS=" + worstFrameFps.ToString() + " WorstTimeslice=" + m_WorstTimeslice.ToString() + " Players=" + players.ToString() + " Frames=" + m_FrameCount.ToString();
             EoH_LiveAdvisorLogger.Log("PERFORMANCE_SAMPLE", sampleMessage, "info", "EoH_Performance");
         }
 
-        string activityMessage = "FPS=" + fps.ToString() + " Players=" + players.ToString();
+        string activityMessage = "AvgFPS=" + avgFps.ToString() + " WorstFrameFPS=" + worstFrameFps.ToString() + " Players=" + players.ToString();
         EoH_LiveAdvisorActivity.LogActivity("performance_monitor", activityMessage);
 
         float now = GetGame().GetTime() / 1000.0;
         float cooldown = EoH_LiveAdvisorLogger.m_Config.PerformanceWarningCooldownSeconds;
 
-        if (m_LastPerformanceWarningTime > 0 && now - m_LastPerformanceWarningTime < cooldown)
+        if (m_LastPerformanceWarningTime <= 0 || now - m_LastPerformanceWarningTime >= cooldown)
         {
-            return;
+            if (avgFps <= EoH_LiveAdvisorLogger.m_Config.CriticalFpsThreshold || worstFrameFps <= EoH_LiveAdvisorLogger.m_Config.CriticalFpsThreshold)
+            {
+                m_LastPerformanceWarningTime = now;
+                string criticalMessage = "CRITICAL server FPS detected. AvgFPS=" + avgFps.ToString() + " WorstFrameFPS=" + worstFrameFps.ToString() + " WorstTimeslice=" + m_WorstTimeslice.ToString() + " ActivePlayers=" + players.ToString();
+                EoH_LiveAdvisorLogger.Log("SERVER_FPS_CRITICAL", criticalMessage, "critical", "EoH_Performance");
+            }
+            else if (avgFps <= EoH_LiveAdvisorLogger.m_Config.LowFpsWarningThreshold || worstFrameFps <= EoH_LiveAdvisorLogger.m_Config.LowFpsWarningThreshold)
+            {
+                m_LastPerformanceWarningTime = now;
+                string warningMessage = "Low server FPS detected. AvgFPS=" + avgFps.ToString() + " WorstFrameFPS=" + worstFrameFps.ToString() + " WorstTimeslice=" + m_WorstTimeslice.ToString() + " ActivePlayers=" + players.ToString();
+                EoH_LiveAdvisorLogger.Log("SERVER_FPS_WARNING", warningMessage, "warning", "EoH_Performance");
+            }
         }
 
-        if (fps <= EoH_LiveAdvisorLogger.m_Config.CriticalFpsThreshold)
-        {
-            m_LastPerformanceWarningTime = now;
+        ResetSampleWindow();
+    }
 
-            string criticalMessage = "CRITICAL server FPS detected. FPS=" + fps.ToString() + " Timeslice=" + timeslice.ToString() + " ActivePlayers=" + players.ToString();
-
-            EoH_LiveAdvisorLogger.Log("SERVER_FPS_CRITICAL", criticalMessage, "critical", "EoH_Performance");
-            return;
-        }
-
-        if (fps <= EoH_LiveAdvisorLogger.m_Config.LowFpsWarningThreshold)
-        {
-            m_LastPerformanceWarningTime = now;
-
-            string warningMessage = "Low server FPS detected. FPS=" + fps.ToString() + " Timeslice=" + timeslice.ToString() + " ActivePlayers=" + players.ToString();
-
-            EoH_LiveAdvisorLogger.Log("SERVER_FPS_WARNING", warningMessage, "warning", "EoH_Performance");
-        }
+    static void ResetSampleWindow()
+    {
+        m_SampleTimer = 0;
+        m_AccumulatedTime = 0;
+        m_FrameCount = 0;
+        m_WorstTimeslice = 0;
     }
 
     static int EoH_GetPlayerCount()
