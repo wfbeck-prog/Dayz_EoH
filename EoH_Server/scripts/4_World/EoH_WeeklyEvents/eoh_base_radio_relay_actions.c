db7e41ff85d1f6ebeb636bcd37f0c5f5da403062
wfbeck-prog <wfbeck@gmail.com>
2026-05-29 16:48:40 -0700

    Register relay repair action on radio relay base classes

---
+++ EoH_Server/scripts/4_World/EoH_WeeklyEvents/eoh_base_radio_relay_actions.c
@@ -0,0 +1,17 @@
+modded class BaseRadio
+{
+    override void SetActions()
+    {
+        super.SetActions();
+        AddAction(ActionRepairAltarRelayTerminal);
+    }
+}
+
+modded class EoH_RadioRelay
+{
+    override void SetActions()
+    {
+        super.SetActions();
+        AddAction(ActionRepairAltarRelayTerminal);
+    }
+}
