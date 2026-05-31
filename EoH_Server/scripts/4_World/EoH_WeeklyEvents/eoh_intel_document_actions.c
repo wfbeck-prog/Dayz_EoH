f33d2e8f18d4503c910922fd60955f19b6f86544
wfbeck-prog <wfbeck@gmail.com>
2026-05-31 16:54:23 -0700

    Disable broad intel action override

--- EoH_Server/scripts/4_World/EoH_WeeklyEvents/eoh_intel_document_actions.c
+++ EoH_Server/scripts/4_World/EoH_WeeklyEvents/eoh_intel_document_actions.c
@@ -1,10 +1,7 @@
-modded class ItemBase
-{
-    override void SetActions()
-    {
-        super.SetActions();
-
-        AddAction(ActionUseWeekendEventIntel);
-        AddAction(ActionUseAltarRelayIntel);
-    }
-}
+// Disabled intentionally.
+// Do not register weekend intel actions globally on ItemBase.
+// The broad ItemBase SetActions override can interfere with the existing EoH intel framework.
+// Weekend/Altar event intel should be wired through the existing files:
+// - EoH_Server/scripts/4_World/eoh_intel_item.c
+// - EoH_Server/scripts/4_World/eoh_intel_read_action.c
+// - EoH_Server/scripts/4_World/eoh_playerbase_intel.c
