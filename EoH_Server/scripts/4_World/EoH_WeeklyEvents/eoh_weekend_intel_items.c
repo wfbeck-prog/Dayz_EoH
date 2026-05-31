49fffeeb13184aac1c7057c992c76333008d7d01
wfbeck-prog <wfbeck@gmail.com>
2026-05-31 15:03:06 -0700

    Register weekend intel item actions

---
+++ EoH_Server/scripts/4_World/EoH_WeeklyEvents/eoh_weekend_intel_items.c
@@ -0,0 +1,17 @@
+modded class EoH_WeekendEventIntel
+{
+    override void SetActions()
+    {
+        super.SetActions();
+        AddAction(ActionUseWeekendEventIntel);
+    }
+}
+
+modded class EoH_AltarRelayIntel
+{
+    override void SetActions()
+    {
+        super.SetActions();
+        AddAction(ActionUseAltarRelayIntel);
+    }
+}
