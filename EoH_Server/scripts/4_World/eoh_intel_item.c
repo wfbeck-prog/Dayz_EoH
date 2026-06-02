bcab2fa4c43046d28688fce808a409b3ba41fd86
wfbeck-prog <wfbeck@gmail.com>
2026-06-02 14:27:51 -0700

    Use shared read action on weekend intel item

--- EoH_Server/scripts/4_World/eoh_intel_item.c
+++ EoH_Server/scripts/4_World/eoh_intel_item.c
@@ -40,6 +40,6 @@ class EoH_WeekendEventIntel : EoH_Intel_Document
     override void SetActions()
     {
         super.SetActions();
-        AddAction(ActionUseWeekendEventIntel);
+        AddAction(EoH_ActionReadIntel);
     }
 }
